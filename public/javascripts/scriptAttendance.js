e3c85c8eddfeb034591535ece36ffd03b4c31021
Alla02 <agotolova@mail.ru>
2019-03-25 04:17:08 +0800

    some unfinished changes (editpage, attendance page)

---
+++ public/javascripts/scriptAttendance.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+    $(function(){
+        moment.locale('ru');
+        $('#day').daterangepicker({
+            singleDatePicker: true,
+            showDropdowns: true,
+            locale: {
+                format: 'YYYY-MM-DD'
+            },
+        });
+        $('#day').val('');
+        $('#day').attr("placeholder","Выбрать день");
+    });
+    
+    function attendance() {
+        var url = window.location.pathname;
+        var groupId_ = url.substring(url.lastIndexOf('/') + 1);
+        $.ajax({
+            type: "POST",
+            url: "/fillAttendance",
+            data: jQuery.param({id_group: groupId_}),
+            dataType: "json"
+        }).done(function (data) {
+            let table = document.getElementById("table1");
+            var dayOfWeek = [];
+            var subjectName = [];
+            var day1= 0; var day2= 0; var day3= 0;
+            var day4= 0; var day5= 0; var day6= 0;
+            for (var i in data) {
+                dayOfWeek.push(data[i].dayOfWeek);
+                subjectName.push(data[i].subjectName);
+                //подсчитываем количество предметов за один день для настройки объединения
+                if (data[i].dayOfWeek==1) day1 = day1+1;
+                if (data[i].dayOfWeek==2) day2 = day2+1;
+                if (data[i].dayOfWeek==3) day3 = day3+1;
+                if (data[i].dayOfWeek==4) day4 = day4+1;
+                if (data[i].dayOfWeek==5) day5 = day5+1;
+                if (data[i].dayOfWeek==6) day6 = day6+1;
+            }
+            if (dayOfWeek.length){
+                $([Понедельник]).attr('colspan',day1);
+                $([Вторник]).attr('colspan',day2);
+                $([Среда]).attr('colspan',day3);
+                $([Четверг]).attr('colspan',day4);
+                $([Пятница]).attr('colspan',day5);
+                $([Суббота]).attr('colspan',day6);
+                $('#subjects').append('<td></td>');
+                for (var i in data) {
+
+               // $('#Понедельник').append("<tr><td>1</td><td>Thomas</td></tr>");
+                if (data[i].typesubject=="практика") $('#subjects').append('<td id="'+data[i].subjectId+'"class="typesPractice">'+data[i].subjectName+'</td>');
+                else $('#subjects').append('<td id="'+data[i].subjectId+'"class="typesLection">'+data[i].subjectName+'</td>');
+                $('.check').append('<td><input type="checkbox"></td>');
+                $('.check2').append('<td><input type="checkbox" class="chkParent"></td>');
+
+                }
+                $('#table1 input:checkbox').change(function(e) {
+                    if(this.checked) {
+                        var value = parseInt($(this).closest('td').next('td').text(), 10);
+                        //alert(value);
+                        // above will convert it from a string to an integer
+                    }
+                    else {
+                        // same as above? Seems redundant
+                    }
+                });
+
+                //$("#table1 tr td:nth-child(1) input[type=checkbox]").prop("checked", true);
+
+                $('.chkParent').on("change", function() {
+                    var $cb = $(this),
+                        $th = $cb.closest("td"), // get parent th
+                        col = $th.index() + 1;  // get column index. note nth-child starts at 1, not zero
+                    alert(col);
+                    $("tbody td:nth-child(" + col + ") input").prop("checked", this.checked);  //select the inputs and [un]check it
+                });
+            }
+        });
+    };
+    attendance();
+});
+
+/*
+<form method="POST" enctype="multipart/form-data" >
+    <input type="hidden" name="chk" value="update">
+    <table border="groove" cellpadding="15px">
+<tr>
+<td>s.no</td>
+<td>Reg. No</td>
+<td>name</td>
+<td>Present</td>
+</tr>
+
+<?php
+    include_once("yourconfig.php"); //add here your db config file
+extract($_POST);
+//After Click on Submit Call this
+if(isset($btnAbsent))
+{
+    foreach($attend as $atn_key=>$atn_value)
+    {
+        if($atn_value=="present")
+        {
+            $upd_qry="UPDATE attendance SET present=present+1 where s_no='".$atn_key."'";
+            mysql_query($upd_qry);
+        }
+        elseif($atn_value=="absent")
+        {
+            $upd_qry="UPDATE attendance SET absent=absent-1 where s_no='".$atn_key."'";
+            mysql_query($upd_qry);
+        }
+    }
+}
+
+
+//Default call this
+$check_exist_qry="select * from attendance";
+$rs=mysql_query($check_exist_qry);
+$total_found=mysql_num_rows($rs);
+while ($row = mysql_fetch_assoc($rs))
+{
+    $id=  $row['s_no'];
+    $no[]=  $row['std_reg_no'];
+    echo "<tr><td>";
+    echo $row['s_no']."</td><td>";
+    echo $row['std_reg_no']."</td><td>";
+    echo $row['std_name']."</td>";
+    echo "<td> <input type='radio' name='attend[$id]' value='present' >Present &nbsp; <input type='radio' name='attend[$id]' value='absent'>absent</td></tr>";
+}
+
+echo "</table>";
+echo "<input type='submit' name='btnAbsent' value='submit'>";
+    ?>
+</form>*/
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -777,4 +777,308 @@ router.post("/regStudents", function(req, res, next) {
     });
 });
 
+router.get('/table', function(req, res, next) {
+    var studyGroups = [];
+    pool.getConnection(function(err, db) {
+        if (err) return next(err); // not connected!
+        //db.query("SELECT column_type FROM information_schema.columns WHERE table_name = 'users' AND column_name = 'typeUser';", (err, rows) => {
+        db.query("SELECT * FROM studyGroups ORDER BY name", (err, rows) => {
+            if (err) {
+                return next(err);
+            }
+            rows.forEach(row => {
+                studyGroups.push({ id: row.id, name: row.name });
+            });
+            var weekday = [];
+            db.query("SELECT * FROM weekdays", (err, rows) => {
+                if (err) {
+                    return next(err);
+                }
+                rows.forEach(row => {
+                    weekday.push({ id: row.id, name: row.name });
+                });
+                var times = [];
+                db.query("SELECT * FROM time", (err, rows) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    rows.forEach(row => {
+                        times.push({ id: row.id, time: row.time });
+                    });
+                    var subjects = [];
+                    db.query("SELECT * FROM subjects ORDER BY name", (err, rows) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        rows.forEach(row => {
+                            subjects.push({ id: row.id, name: row.name });
+                        });
+                        var teachers = [];
+                        db.query("SELECT * FROM persons WHERE id IN (SELECT id_person FROM teachers)", (err, rows) => {
+                            if (err) {
+                                return next(err);
+                            }
+                            rows.forEach(row => {
+                                teachers.push({ id: row.id, firstname: row.first_name, secondname: row.second_name, lastname: row.flast_name });
+                            });
+                            var typesubject = [];
+                            db.query("SELECT * FROM typesubject", (err, rows) => {
+                                if (err) {
+                                    return next(err);
+                                }
+                                rows.forEach(row => {
+                                    typesubject.push({ id: row.id, name: row.name });
+                                });
+                                var typeweek = [];
+                                db.query("SELECT * FROM typeweek", (err, rows) => {
+                                    if (err) {
+                                        return next(err);
+                                    }
+                                    rows.forEach(row => {
+                                        typeweek.push({ id: row.id, name: row.name });
+                                    });
+                                    var semester = [];
+                                    db.query("SELECT * FROM semester", (err, rows) => {
+                                        if (err) {
+                                            return next(err);
+                                        }
+                                        rows.forEach(row => {
+                                            semester.push({ id: row.id, start: row.start, end: row.end, name: row.name });
+                                        });
+                                    var login,lastname,secondname,firstname,type_user,email = "";
+                                    if (req.user){
+                                        login = req.user.login;
+                                        lastname = req.user.last_name;
+                                        firstname = req.user.first_name;
+                                        type_user = req.user.user_type;
+                                        email = req.user.email;
+                                        secondname = req.user.second_name;
+                                    }
+                                    res.render("scheduleEdit", {title: "Редактирование распиания",
+                                        studyGroups: studyGroups,
+                                        weekday: weekday,
+                                        times: times,
+                                        subjects: subjects,
+                                        teachers: teachers,
+                                        typesubject: typesubject,
+                                        typeweek: typeweek,
+                                        semester: semester,
+                                        login: login,
+                                        lastname: lastname,
+                                        firstname: firstname,
+                                        secondname: secondname,
+                                        type_user: type_user,
+                                        email: email,
+                                        message: req.flash("registerMessage") });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+            db.release();
+            if (err) return next(err);
+        });
+    });
+});
+
+router.post("/table", function(req, res, next) {
+    pool.getConnection(function(err, db) {
+        if (err) return next(err);
+        db.query("SELECT id FROM teachers WHERE id_person=?;",[req.body.teacher], function (err, row) {
+            if (err) return next(err);
+            else { var idTeacher = row[0].id;
+                db.query(`INSERT INTO subjteacher(id_subject, id_teacher, type_subject) VALUES (?,?,?);`,[req.body.subject,idTeacher,req.body.typeSubject], err => {
+                    if (err) return next(err);
+                    else {
+                        db.query("SELECT id FROM subjteacher ORDER BY id DESC LIMIT 1;", function (err, row) {
+                            if (err) return next(err);
+                            var idSubjTeacher = row[0].id;
+                            if (idSubjTeacher != 0) {
+                                db.query("INSERT into schedule (id_group,dayOfWeek,numPair,typeWeek,id_subjteacher,id_semester) values (?,?,?,?,?,?);", [req.body.selectStudyGroup, req.body.weekday, req.body.time, req.body.typeWeek, idSubjTeacher, req.body.semester], function (err) {
+                                    if (err) {
+                                        console.log(err);
+                                    }
+                                    console.log("inserted into schedule");
+                                })
+                            }
+                        });
+                    }
+                    res.redirect("/table");
+                });
+            }
+        });
+        db.release();
+        if (err) return next(err);
+    });
+});
+
+router.get('/attendance/:id', function(req, res, next) {
+    var curGroup = []; var listStudents = [];
+    pool.getConnection(function(err, db) {
+        if (err) return next(err);
+        var login,lastname,secondname,firstname,type_user,email = "";
+        if (req.user){
+            login = req.user.login;
+            lastname = req.user.last_name;
+            firstname = req.user.first_name;
+            type_user = req.user.user_type;
+            email = req.user.email;
+            secondname = req.user.second_name;
+        }
+        db.query("SELECT * FROM weekdays", (err, rows) => {
+            if (err) return next(err);
+            var weekday = [];0
+            rows.forEach(row => {
+                weekday.push({ id: row.id, name: row.name });
+            });
+            var times = [];
+            db.query("SELECT * FROM time", (err, rows) => {
+                if (err) return next(err);
+                rows.forEach(row => {
+                    times.push({ id: row.id, time: row.time });
+                });
+                console.log(req.params.id);
+                db.query("SELECT * FROM persons WHERE id IN (SELECT id_person FROM students WHERE id_group=?)",req.params.id, (err, rows) => {
+                    if (err) return next(err);
+                    rows.forEach(row => {
+                        listStudents.push({ id: row.id, firstname: row.first_name, lastname: row.last_name, secondname: row.second_name, birthyear: row.birthyear });
+                    });
+                    if (listStudents.length==0) res.render("attendance", {title: "Посещаемость группы",weekday: weekday,times: times,listStudents:listStudents,login: login,
+                        lastname: lastname,firstname: firstname,secondname: secondname,type_user: type_user,email: email,
+                        message: req.flash("У этой группы нет студентов") });
+                    else {
+                        db.query("SELECT * FROM schedule WHERE id_group=?",req.params.id, (err, rows) => {
+                            if (err) return next(err);
+                            if (rows) res.render("attendance", {title: "Посещаемость группы",weekday: weekday,
+                                times: times,listStudents: listStudents,login: login,
+                                lastname: lastname,firstname: firstname,secondname: secondname,type_user: type_user,email: email,
+                                message: req.flash("Для этой группы нет занятий") });
+                            else {
+                                rows.forEach(row => {
+                                    curGroup.push({ id: row.id, dayOfWeek: row.dayOfWeek, numPar: row.numPair, typeWeek: row.typeWeek, subjteacher: row.id_subjteacher, semester: row.id_semester, teacher: row.id_teacher, subject: row.idsubject });
+                                });
+                                var subjteachersarr = curGroup.map(function (el) { return el.subjteacher; });
+                                var subjects = [];
+                                //db.query("SELECT name from subjects WHERE id IN (SELECT id_subject FROM subjteacher WHERE id In ("+subjteachersarr+"));", (err, rows) => {
+                                db.query("SELECT name from subjects WHERE id IN (SELECT id_subject FROM subjteacher WHERE id In ("+subjteachersarr+"));", (err, rows) => {
+                                    if (err) return next(err);
+                                    console.log(rows);
+                                    /*
+                                    rows.forEach(row => {
+                                        subjects.push({ id: row.id, name: row.name });
+                                    });*/
+                                    var teachers = [];
+                                    db.query("SELECT * FROM persons WHERE id IN (SELECT id_person FROM teachers)", (err, rows) => {
+                                        if (err) return next(err);
+                                        rows.forEach(row => {
+                                            teachers.push({ id: row.id, firstname: row.first_name, secondname: row.second_name, lastname: row.flast_name });
+                                        });
+                                        var typesubject = [];
+                                        db.query("SELECT * FROM typesubject", (err, rows) => {
+                                            if (err) {
+                                                return next(err);
+                                            }
+                                            rows.forEach(row => {
+                                                typesubject.push({ id: row.id, name: row.name });
+                                            });
+                                            var typeweek = [];
+                                            db.query("SELECT * FROM typeweek", (err, rows) => {
+                                                if (err) {
+                                                    return next(err);
+                                                }
+                                                rows.forEach(row => {
+                                                    typeweek.push({ id: row.id, name: row.name });
+                                                });
+                                                var semester = [];
+                                                db.query("SELECT * FROM semester", (err, rows) => {
+                                                    if (err) {
+                                                        return next(err);
+                                                    }
+                                                    rows.forEach(row => {
+                                                        semester.push({ id: row.id, start: row.start, end: row.end, name: row.name });
+                                                    });
+                                                    res.render("attendance", {title: "Посещаемость группы",
+                                                        weekday: weekday,
+                                                        times: times,
+                                                        subjects: subjects,
+                                                        teachers: teachers,
+                                                        typesubject: typesubject,
+                                                        typeweek: typeweek,
+                                                        semester: semester,
+                                                        listStudents: listStudents,
+                                                        login: login,
+                                                        lastname: lastname,
+                                                        firstname: firstname,
+                                                        secondname: secondname,
+                                                        type_user: type_user,
+                                                        email: email,
+                                                        message: req.flash("registerMessage") });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+            db.release();
+            if (err) return next(err);
+        });
+    });
+});
+
+router.post("/fillAttendance", function(req, res, next) {
+    //req.body.group  - AJAX data from /table
+    var result = [];
+    let str = `SELECT schedule.id, id_group, studyGroups.name as groupName,dayOfWeek, numPair,
+    schedule.id_teacher as id_teacher, persons.second_name as secondname, persons.last_name as lastname, 
+    persons.first_name as firstname, schedule.id_subject as id_subject, subjects.name as subjectName, typeWeek, subjteacher.type_subject as typesubject
+    FROM schedule 
+    INNER JOIN studyGroups ON studyGroups.id=schedule.id_group
+    INNER JOIN teachers ON teachers.id=schedule.id_teacher  
+    INNER JOIN subjects ON subjects.id=schedule.id_subject
+    INNER JOIN persons ON persons.id=teachers.id_person
+    INNER JOIN subjteacher ON subjteacher.id=schedule.id_subjteacher
+    WHERE id_group=3`;
+    console.log("blablabla");
+    pool.getConnection(function(err, db) {
+        if (err) return next(err); // not connected!
+        db.query(str, [req.body.id_group], (err, rows) => {
+            console.log(rows);
+            if (err) {
+                return next(err);
+            } else {
+                rows.forEach(row => {
+                    result.push({
+                        id: row.id,
+                        dayOfWeek: row.dayOfWeek,
+                        numPair: row.numPair,
+                        groupId: row.id_group,
+                        groupName: row.groupName,
+                        teacherId: row.id_teacher,
+                        teacherName:
+                            row.lastname +
+                            " " +
+                            row.firstname +
+                            " " +
+                            row.secondname,
+                        subjectId: row.id_subject,
+                        subjectName: row.subjectName,
+                        week: row.typeWeek,
+                        typesubject: row.typesubject
+                    });
+                });
+            }
+            res.send(JSON.stringify(result));
+            db.release();
+            if (err) return next(err);
+            // Don't use the db here, it has been returned to the pool.
+        });
+    });
+});
+
 module.exports = router;
